ce39ec9dd765cc96a07b1d3fb2b96317a4324def
MarkHaihel <markiyanse@gmail.com>
2023-01-07 23:58:14 +0200

    End of section 18

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3a0a13b9-4c93-4177-af7b-fb7594c33d94';
+export const MODAL_CLOSE_SEC = 2.5;
